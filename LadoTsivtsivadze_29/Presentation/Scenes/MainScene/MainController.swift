a9432b3cc081548783af0c1ee2b57964a4b96165
Lado <Tsivtsivadzelado@yahoo.com>
2021-06-09 04:15:15 -0700

    tested pan gesture recognizer

--- LadoTsivtsivadze_29/Presentation/Scenes/MainScene/MainController.swift
+++ LadoTsivtsivadze_29/Presentation/Scenes/MainScene/MainController.swift
@@ -13,7 +13,9 @@ class MainController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configTapGesture()
+        imgView.isUserInteractionEnabled = true
+        //configTapGesture()
+        configPanGesture()
         
         
     }
@@ -22,11 +24,33 @@ class MainController: UIViewController {
         let tap = UITapGestureRecognizer(target: self, action: #selector(tapAction))
         imgView.addGestureRecognizer(tap)
     }
+    
+    func configPanGesture() {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(panAction))
+        view.addGestureRecognizer(pan)
+    }
 }
 
 extension MainController {
     @objc func tapAction(_ gesture: UITapGestureRecognizer) {
         print("did tap")
+    }
+    
+    @objc func panAction(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: self.view)
+
+        switch gesture.state {
+        case .began:
+            print("begin")
+        case .changed:
+            self.imgView.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        case .ended:
+            UIView.animate(withDuration: 0.3) {
+                self.imgView.transform = .identity
+            }
+        default:
+            break
+        }
     }
 }
 
