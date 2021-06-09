a2336ab00fa49aa071a8cff49eb85d8942674190
Lado <Tsivtsivadzelado@yahoo.com>
2021-06-09 03:17:17 -0700

    Initial Commit

---
+++ LadoTsivtsivadze_29/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LadoTsivtsivadze_29
+//
+//  Created by Ladolado3911 on 6/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
